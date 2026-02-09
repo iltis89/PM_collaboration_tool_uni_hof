c78f80edc8f017099b54e8b84a96726caa427adc
Marcus Görner <marcus.goerner@requestchange.eu>
2026-02-09 14:45:42 +0100

    fix: CSP-Header erlaubt jetzt Google Fonts für Material Icons
    - font-src: fonts.gstatic.com hinzugefügt (Font-Dateien)
    - style-src: fonts.googleapis.com hinzugefügt (CSS-Stylesheet)
    - connect-src: fonts.googleapis.com hinzugefügt
    - Ursache: Icons wurden als Text gerendert weil CSP die Font blockierte

--- next.config.ts
+++ next.config.ts
@@ -32,10 +32,10 @@ const nextConfig: NextConfig = {
             value: [
               "default-src 'self'",
               "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
-              "style-src 'self' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' blob: data: https://dfibivartddugsms.public.blob.vercel-storage.com",
-              "font-src 'self'",
-              "connect-src 'self'",
+              "font-src 'self' https://fonts.gstatic.com",
+              "connect-src 'self' https://fonts.googleapis.com",
               "frame-ancestors 'none'",
             ].join('; '),
           },
